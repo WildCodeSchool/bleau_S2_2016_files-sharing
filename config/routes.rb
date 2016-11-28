5e1b4b8d6775f199f4f2cf292ee73281b72e83c9
michaelcombescot <michael.combescot@gmail.com>
2016-11-28 23:25:46 +0100

    page accueil premier jet

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :groups
   resources :media
   resources :users
-  root 'main#index'
+  root 'main#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
