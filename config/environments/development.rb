53cf7baf8c1ebf8e6dcf4216736f67467a485ba1
michaelcombescot <michael.combescot@gmail.com>
2017-01-13 17:31:19 +0100

    visu erreur heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
   enable_starttls_auto: true
 }
 
+  config.consider_all_requests_local = true
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
