fc201f5c490a5742fdf4ad5800a232a297566641
michaelcombescot <michael.combescot@gmail.com>
2017-01-30 11:34:43 +0100

    ajout pagination sur la recherche, prochaine étape mise en page de l'edit de profile et la création de média

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,15 +7,15 @@ class MainController < ApplicationController
 
 	def search_files
 		if params[:select] == "All"
-			@media = Medium.search_all(params[:search]).where(visible_to_all: true)
+			@media = Medium.search_all(params[:search]).where(visible_to_all: true).page params[:page]
 		elsif params[:select] == "My files"
-			@media = Medium.search_in_my_files(current_user, params[:search])
+			@media = Medium.search_in_my_files(current_user, params[:search]).page params[:page]
 		elsif params[:select] == "Shared by my groups"
-			@media = Medium.search_in_files_shared_by_my_groups(current_user, params[:search]).group_by_id
+			@media = Medium.search_in_files_shared_by_my_groups(current_user, params[:search]).group_by_id.page params[:page]
 		elsif params[:select] == "Shared with me"
-			@media = Medium.search_in_files_shared_with_me(current_user, params[:search]).group_by_id
+			@media = Medium.search_in_files_shared_with_me(current_user, params[:search]).group_by_id.page params[:page]
 		elsif params[:select] == "All files downloadable"
-			@media = Medium.search_all_files_downloadable(current_user, params[:search])
+			@media = Medium.search_all_files_downloadable(current_user, params[:search]).page params[:page]
 		end
 	end
 
