39ceddaf4d7f3127836d7a81691dfbfd7e82eaa4
michaelcombescot <michael.combescot@gmail.com>
2017-02-01 17:20:00 +0100

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,6 @@ Rails.application.configure do
   config.assets.css_compressor = :yui
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.compress = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
